475261cf5c8e27e71ccf0d64a51a681d7f417c83
Marc Portier <marc.portier@gmail.com>
2022-04-19 15:46:41 +0200

    Initial commit

---
+++ entrypoint.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env -S python3 -B
+
+# NOTE: If you are using an alpine docker image
+# such as pyaction-lite, the -S option above won't
+# work. The above line works fine on other linux distributions
+# such as debian, etc, so the above line will work fine
+# if you use pyaction:4.0.0 or higher as your base docker image.
+
+import sys
+
+if __name__ == "__main__" :
+    # Rename these variables to something meaningful
+    input1 = sys.argv[1]
+    input2 = sys.argv[2]
+
+
+    # Fake example outputs
+    output1 = "Hello"
+    output2 = "World"
+
+    # This is how you produce outputs.
+    # Make sure corresponds to output variable names in action.yml
+    print("::set-output name=output-one::" + output1)
+    print("::set-output name=output-two::" + output2)
