2fc9f144dc3d9efe6311de03e771f61e35f8882e
Marc Portier <marc.portier@gmail.com>
2022-07-22 10:33:01 +0200

    this is what you get from a sloppy diff check pre-commit

--- tests/integration.py
+++ tests/integration.py
@@ -1,5 +1,4 @@
-from assert_outcome import
-from tests.assert_outcome import assert_result 
+from assert_outcome import assert_result 
 
 def test_integration_run_result():
     assert_result("http://example.org/integration-test", "tests/out", {"sub/onto-two.ttl", "onto-one.ttl"})
