a4fc32bbb44d6f5c763a6e6969a4c896fe38593a
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2023-02-15 10:39:26 +0100

    fixed bug where all ontologies were having a vocabulary

--- tests/test_entrypoint.py
+++ tests/test_entrypoint.py
@@ -42,7 +42,7 @@ def test_main():
     baseuri = 'https://example.org/pylode2pages-test'
     ontos = ep.publish_ontologies(baseuri, str(nsfolder), str(outfolder), "templates")
     vocabs = ep.publish_vocabs(baseuri, str(nsfolder), str(outfolder), "templates")
-    ep.publish_combined_index(baseuri, str(nsfolder), str(outfolder), "templates")
+    ep.publish_index_html(baseuri, str(nsfolder), str(outfolder), "templates", ontos, vocabs)
     log.info(f"ontologies produced == {ontos}")
 
     # assert returned list of ontologies processed.
